b3279f699d2614e3003842a4b83fab8ff45121be
Денис <whitechapel5991@gmail.com>
2020-07-26 22:21:00 +0300

    init

---
+++ IdentityServer4/IdentityServer/IdentityServer.Infrastructure/Constants/Roles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityServer.Infrastructure.Constants
+{
+    class Roles
+    {
+        public const string Consumer = "consumer";
+    }
+}
---
+++ IdentityServer4/IdentityServer/IdentityServer.Infrastructure/Data/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityServer.Infrastructure.Data.Identity
+{
+    class AppUser : IdentityUser
+    {
+    }
+}
